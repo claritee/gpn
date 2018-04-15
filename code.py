c8c7d72b43a2f2fb151a9bcf510a8bb9940ab733
Claire Tran <c.tran83@gmail.com>
2018-04-15 21:35:00 +1000

    adding section 5

--- code.py
+++ code.py
@@ -15,26 +15,28 @@ name = input("What is your name? ")
 print("Hello " + name.lower())
 
 moves = ['paper', 'scissors', 'rock']
-computer_move = random.choice(moves)
-human_move = input("What is your next move? scissors, paper or rock? ")
 
-print("Computer played: " + computer_move)
-time.sleep(0.1)
+while True:
+	computer_move = random.choice(moves)
+	human_move = input("What is your next move? scissors, paper or rock? ")
 
-print("Human played: " + human_move)
-time.sleep(0.1)
+	print("Computer played: " + computer_move)
+	time.sleep(0.1)
 
-if computer_move == "paper" and human_move == "scissors":
-	print("Human won the round!")
-elif computer_move == "paper" and human_move == "rock":
-	print("Computer won the round!")
-elif computer_move == "scissors" and human_move == "rock":
-	print("Human won the round!")
-elif computer_move == "scissors" and human_move == "paper":
-	print("Computer won the round!")
-elif computer_move == "rock" and human_move == "paper":
-	print("Human won the round!")
-elif computer_move == "rock" and human_move == "scissors":
-	print("Computer won the round!")
-else:
-	print("Both played the same move")
+	print("Human played: " + human_move)
+	time.sleep(0.1)
+
+	if computer_move == "paper" and human_move == "scissors":
+		print("Human won the round!")
+	elif computer_move == "paper" and human_move == "rock":
+		print("Computer won the round!")
+	elif computer_move == "scissors" and human_move == "rock":
+		print("Human won the round!")
+	elif computer_move == "scissors" and human_move == "paper":
+		print("Computer won the round!")
+	elif computer_move == "rock" and human_move == "paper":
+		print("Human won the round!")
+	elif computer_move == "rock" and human_move == "scissors":
+		print("Computer won the round!")
+	else:
+		print("Both played the same move")
