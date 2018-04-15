80eb5cfff2874629ee26178223ebbd74f5866042
Claire Tran <c.tran83@gmail.com>
2018-04-15 21:32:14 +1000

    Adding section4

--- code.py
+++ code.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import time
+import random
 
 print("-------------------------------------------------------")
 print ("Welcome to Human vs. Computer in Scissors, Paper, Rock!")
@@ -11,9 +12,10 @@ print("Good luck!")
 print("------------\n")
 
 name = input("What is your name? ")
-print("Hello " + name)
+print("Hello " + name.lower())
 
-computer_move = 'paper'
+moves = ['paper', 'scissors', 'rock']
+computer_move = random.choice(moves)
 human_move = input("What is your next move? scissors, paper or rock? ")
 
 print("Computer played: " + computer_move)
@@ -24,5 +26,15 @@ time.sleep(0.1)
 
 if computer_move == "paper" and human_move == "scissors":
 	print("Human won the round!")
-else if computer_move == "paper" and human_move == "rock":
+elif computer_move == "paper" and human_move == "rock":
 	print("Computer won the round!")
+elif computer_move == "scissors" and human_move == "rock":
+	print("Human won the round!")
+elif computer_move == "scissors" and human_move == "paper":
+	print("Computer won the round!")
+elif computer_move == "rock" and human_move == "paper":
+	print("Human won the round!")
+elif computer_move == "rock" and human_move == "scissors":
+	print("Computer won the round!")
+else:
+	print("Both played the same move")
