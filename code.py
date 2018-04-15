7bf41129b5db59318dc5cce275aeaeeceaede6e4
Claire Tran <c.tran83@gmail.com>
2018-04-15 21:19:24 +1000

    adding intermediate section 2

---
+++ code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+print("-------------------------------------------------------")
+print ("Welcome to Human vs. Computer in Scissors, Paper, Rock!")
+print("-------------------------------------------------------")
+print("Moves: choose scissors, paper or rock by typing in your selection.")
+print("Rules: scissors cuts paper, paper covers rock and rock crushes scissors.")
+print("Good luck!")
+print("------------\n")
+
+computer_move = 'paper'
+move = input("What is your next move? scissors, paper or rock? ")
+
+print("Computer played: " + computer_move)
+print("Human played: " + move)
