abd499466f38d5c36f294f558868eea4cb5a55da
Claire Tran <c.tran83@gmail.com>
2018-04-15 21:22:07 +1000

    Adding section 2 bonus

--- code.py
+++ code.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+import time
+
 print("-------------------------------------------------------")
 print ("Welcome to Human vs. Computer in Scissors, Paper, Rock!")
 print("-------------------------------------------------------")
@@ -8,8 +10,14 @@ print("Rules: scissors cuts paper, paper covers rock and rock crushes scissors."
 print("Good luck!")
 print("------------\n")
 
+name = input("What is your name? ")
+print("Hello " + name)
+
 computer_move = 'paper'
 move = input("What is your next move? scissors, paper or rock? ")
 
 print("Computer played: " + computer_move)
+time.sleep(0.1)
+
 print("Human played: " + move)
+time.sleep(0.1)
